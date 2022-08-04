5da25ef19c010b35092061160ffebf4755b6edbd
Dmytro Palianytsia <palianytsia.dmytro@gmail.com>
2022-08-04 15:37:20 +0300

    Add icon for custom image plugin. Fix bug with first initialization

--- src/js/editorjs-image.js
+++ src/js/editorjs-image.js
@@ -8,7 +8,7 @@ export default class CustomImage extends Image  {
     static get toolbox() {
         return {
             title: 'CustomImage',
-            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 3c5.514 0 10 3.592 10 8.007 0 4.917-5.145 7.961-9.91 7.961-1.937 0-3.383-.397-4.394-.644-1 .613-1.595 1.037-4.272 1.82.535-1.373.723-2.748.602-4.265-.838-1-2.025-2.4-2.025-4.872-.001-4.415 4.485-8.007 9.999-8.007zm0-2c-6.338 0-12 4.226-12 10.007 0 2.05.738 4.063 2.047 5.625.055 1.83-1.023 4.456-1.993 6.368 2.602-.47 6.301-1.508 7.978-2.536 1.418.345 2.775.503 4.059.503 7.084 0 11.91-4.837 11.91-9.961-.001-5.811-5.702-10.006-12.001-10.006zm1.024 13.975c0 .566-.458 1.025-1.024 1.025-.565 0-1.024-.459-1.024-1.025 0-.565.459-1.024 1.024-1.024.566 0 1.024.459 1.024 1.024zm1.141-8.192c-.498-.505-1.241-.783-2.09-.783-1.786 0-2.941 1.271-2.941 3.237h1.647c0-1.217.68-1.649 1.261-1.649.519 0 1.07.345 1.117 1.004.052.694-.319 1.046-.788 1.493-1.157 1.1-1.179 1.633-1.173 2.842h1.643c-.01-.544.025-.986.766-1.785.555-.598 1.245-1.342 1.259-2.477.008-.758-.233-1.409-.701-1.882z"/></svg>'
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 350 350" enable-background="new 0 0 350 350"><path d="M5,350h340V0H5V350z M25,330v-62.212h300V330H25z M179.509,247.494H60.491L120,171.253L179.509,247.494z   M176.443,211.061l33.683-32.323l74.654,69.05h-79.67L176.443,211.061z M325,96.574c-6.384,2.269-13.085,3.426-20,3.426  c-33.084,0-60-26.916-60-60c0-6.911,1.156-13.612,3.422-20H325V96.574z M25,20h202.516C225.845,26.479,225,33.166,225,40  c0,44.112,35.888,80,80,80c6.837,0,13.523-0.846,20-2.518v130.306h-10.767l-104.359-96.526l-45.801,43.951L120,138.748  l-85.109,109.04H25V20z"/></svg>'
         };
     }
     rendered(){
@@ -37,21 +37,27 @@ export default class CustomImage extends Image  {
         let imageBlocks = document.querySelectorAll('.image-tool');
         
         let rightBlock;
-        
-        /* Editor js save this type of data separately from each other, so we need to save title to right block. This checking we do by file id for each element */
-        imageBlocks.forEach(block => {
-            const image = block.querySelector('img');
-            const src = image.getAttribute('src');
-            const imageId = src.substring(src.lastIndexOf('/') + 1, src.lastIndexOf('.'));
-            
-            if(Number(imageId) === this._data.file.file_id) {
-                rightBlock = block;
-            }
-        });
+        if (imageBlocks !== null){
 
-        const titleElement = rightBlock.querySelector('.image_title');
+            /* Editor js save this type of data separately from each other, so we need to save title to right block. This checking we do by file id for each element */
+            imageBlocks.forEach(block => {
+                const image = block.querySelector('img');
+                if (image !== null){
+                    const src = image.getAttribute('src');
+                    const imageId = src.substring(src.lastIndexOf('/') + 1, src.lastIndexOf('.'));
+                    
+                    if(Number(imageId) === this._data.file.file_id) {
+                        rightBlock = block;
+                    }
+                }else{
+                    rightBlock = document;
+                }
+            });
+            const titleElement = rightBlock.querySelector('.image_title');
+            this._data.title = titleElement.innerText;
+        }
 
-        this._data.title = titleElement.innerText;
+
 
         return this.data;
       }
