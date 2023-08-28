88eaab7b3b75120fbc62256fdc0ad58ef389c439
Dmytro Palianytsia <palianytsia.dmytro@gmail.com>
2023-08-28 16:45:08 +0300

    Try new parser

--- src/js/parserExtenders.js
+++ src/js/parserExtenders.js
@@ -247,38 +247,13 @@ export const parseHTMLViewer = (block) => {
     // div.querySelector('script').remove();
     
     let script = document.createElement('script');
-    script.innerHTML = block.data.text
+    let a = document.createElement('div')
+    a.innerHTML = block.data.text
+    script.innerHTML = a.querySelector('script')
     div.append(script)
     // return div.innerHTML.replaceAll('&amp;lt;', '&lt;').replaceAll('&amp;gt;', '&gt;').replaceAll('&nbsp;', ' ');
-    function nodeScriptReplace(node) {
-        if ( nodeScriptIs(node) === true ) {
-                node.parentNode.replaceChild( nodeScriptClone(node) , node );
-        }
-        else {
-                var i = -1, children = node.childNodes;
-                while ( ++i < children.length ) {
-                      nodeScriptReplace( children[i] );
-                }
-        }
-
-        return node;
-    }
-    function nodeScriptClone(node){
-            var script  = document.createElement("script");
-            script.text = node.innerHTML;
-
-            var i = -1, attrs = node.attributes, attr;
-            while ( ++i < attrs.length ) {                                    
-                script.setAttribute( (attr = attrs[i]).name, attr.value );
-            }
-            return script;
-    }
-
-    function nodeScriptIs(node) {
-            return node.tagName === 'SCRIPT';
-    }
-    nodeScriptReplace(script);
-    return script.outerHTML;
+   
+    return div.outerHTML;
 }
 export const parseMultiLevelList = (block) => {
     let data = block.data.items;
