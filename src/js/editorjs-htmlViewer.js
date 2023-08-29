896248ce2142d20f2c709efdd87c20e9ed1f24a6
Dmytro Palianytsia <palianytsia.dmytro@gmail.com>
2023-08-29 14:49:50 +0300

    Update getting saved data

--- src/js/editorjs-htmlViewer.js
+++ src/js/editorjs-htmlViewer.js
@@ -6,7 +6,7 @@ export default class LiveCodeEditor extends CodeMirror  {
         this.api = this.savedData.api;
         this.data = {
             language: this.savedData.data.language || 'HTML',
-            text: this.savedData.data.template || this.savedData.data.text?.replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&quot;', '"').replaceAll('&nbsp;', ' ') || ''
+            text: this.savedData.data.template || this.savedData.data.text?.replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&quot;', '"').replaceAll('&nbsp;', ' ').replaceAll('&#39;', "'") || ''
         }
     }
     static get toolbox() {
